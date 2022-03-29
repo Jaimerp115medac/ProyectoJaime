6dbcf8ccc1a6d492fdea31977578f3b9c1697995
jaimerp115 <jrp0010@alu.medac.es>
2022-03-29 17:19:16 +0200

    clase persona actualizada

--- src/t3practica1herencia/Persona.java
+++ src/t3practica1herencia/Persona.java
@@ -1,5 +1,62 @@
 package t3practica1herencia;
 
 public class Persona {
+  private String nombre;
+    private String direccion;
+    private String DNI;
+    protected boolean sexo;//T femenino-F masculino
+
+    //Metodo constructor
+    public Persona(String nombre, String direccion, String DNI, boolean sexo) {
+        this.DNI = DNI;
+        this.sexo = sexo;
+        this.direccion = direccion;
+        this.nombre = nombre;
+    }
+
+    // Constructor copia
+    public Persona(Persona objPersona) {
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        String res;
+        res = this.nombre + " - " + this.DNI;
+        res += "\n";
+        if (sexo) {
+            res += "Es una chica";
+        } else {
+            res += "Es un chico";
+        }
+        res += "\n";
+        res += this.direccion;
+        return res;
+    }
 
 }
+
+
